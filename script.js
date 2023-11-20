82dc53da0a77bf3cc544e2bf67588db0f5e14af3
banaas <abdurahimbana@gmail.com>
2023-11-20 15:51:09 -0500

    Added age calculation and displayed values from script

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const date = new Date();
+
+// Grab the current values of the date
 var currentMonth = date.getMonth();
+currentMonth = currentMonth + 1;
 var currentYear = date.getFullYear();
-var currentDay = date.getDay();
+var currentDay = date.getDate();
 
-var calculate = document.querySelector('.btn btn-primary');
+// Calculate and display the age
 
-var inputYear = document.getElementById('year'); 
-document.getElementById('year').innerHTML = months;
-var newYear = currentMonth - inputYear;
+function ageValue () {
+    var inputYear = document.getElementById("myYear").value; 
+    var inputMonth = document.getElementById("myMonth").value;
+    var inputDay = document.getElementById("myDay").value;
 
-
-function monthValue () {
-    var months = document.getElementById("month").value; 
-    document.getElementById("month").innerHTML = months;
-    var newMonth = currentMonth - months;
-    console.log(newMonth);
+    if (inputYear > currentYear || inputYear <= 0) {
+        alert("You must enter a year before 2023.");
+    } else if (inputMonth < 1 || inputMonth >= 13) {
+        alert("You must enter a month between 1 and 12");
+    } else if (inputDay < 1 || inputDay >= 32) {
+        alert("You must enter a day between 1 and 31");
+    } else {
+        var newYear = currentYear - inputYear;
+        var newMonth = currentMonth - inputMonth;
+        var newDay = currentDay - inputDay;
+        document.getElementById("nYear").innerHTML = newYear + " years";
+        document.getElementById("nMonth").innerHTML = newMonth + " months";
+        document.getElementById("nDays").innerHTML = newDay + " days";
+    }
 }
 
